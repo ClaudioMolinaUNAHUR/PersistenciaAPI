6570624670ddf521a1d15d6a9949a28e9899a03d
Claudio Molina <molina.claudio@estudiantes.unahur.edu.ar>
2022-10-12 22:00:35 -0300

    agregado validacion con bearer

--- api/middleware/verifyToken.js
+++ api/middleware/verifyToken.js
@@ -4,18 +4,24 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 function verifyToken(req, res, next){
-
-    let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+    try{
+    //let tokenHeaderKey = process.env.TOKEN_HEADER_KEY; en el caso de no usar bearer
     let jwtSecretKey = process.env.JWT_SECRET_KEY;
 
-    const token = req.header(tokenHeaderKey);
+    //const token = req.header(tokenHeaderKey); // devuelve value token, pero hay que pasar encabezado valido
+    const bearerHeader = req.headers['authorization'] //key es Authorization, y value "Bearer token"
+    
+    if(typeof bearerHeader !== 'undefined'){
+      const bearer = bearerHeader.split(' ');
+      req.token = bearer[1]; // guarda token en req.token
 
-    if(!token){//validacion token null
-      return res.json({auth: false, messege: "No se ingreso Token"})
     }
-
-    const decoded = jwt.verify(token, jwtSecretKey);
+    
+    const decoded = jwt.verify(req.token, jwtSecretKey);
     req.userId = decoded.id;
     next();
+    }catch{//validacion token null
+      return res.json({auth: false, messege: "No se ingreso Token"})
+    }
 }
 module.exports = verifyToken;
