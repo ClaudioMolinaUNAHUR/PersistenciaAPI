6a799d4e4868047d350210ebb86fc42408a463ef
cberto21 <camila.bertolani@estudiantes.unahur.edu.ar>
2022-11-13 21:13:09 -0300

    cache

--- api/middleware/cache.js
+++ api/middleware/cache.js
@@ -1,5 +1,6 @@
 const getExpeditiousCache = require('express-expeditious');
-
+//guardar en memoria cache las consultas de base de datos  y responder de forma mas rapida las peticiones
+//Un middleware exprés que simplifica el almacenamiento en caché de las respuestas de las solicitudes HTTP de cualquier tipo. 
 const defaultOptions = {
     namespace: 'expresscache',
     defaultTtl: '1 minute', // Almacena entradas de caché durante 1 minuto
